6a679b766f3ad190a101d693c1df67e9b09340ee
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-11-08 23:54:49 +0100

    refactor: add display data of client creation

--- frontend/src/components/ClientItem.jsx
+++ frontend/src/components/ClientItem.jsx
@@ -9,6 +9,7 @@ export default function ClientItem({ client }) {
       "
         >
             <div>
+
                 <p className="text-sm text-gray-500">
                     Company:{' '}
                     <span className="font-semibold text-gray-900">{client.name}</span>
@@ -20,12 +21,16 @@ export default function ClientItem({ client }) {
                 </p>
             </div>
 
-            <p className="text-sm text-gray-500 mt-2 sm:mt-0">
-                Projects:{' '}
-                <span className="font-semibold text-gray-900">
-                    {client.projects?.length ?? 0}
-                </span>
-            </p>
+            <div>
+                <p className="text-xs text-gray-400">{client.acquiredAt}</p>
+                <p className="text-sm text-gray-500 mt-2 sm:mt-0">
+                    Projects:{' '}
+                    <span className="font-semibold text-gray-900">
+                        {client.projects?.length ?? 0}
+                    </span>
+                </p>
+            </div>
+
 
         </li>
 
